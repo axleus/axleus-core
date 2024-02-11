30a54430e414c2ae3e686ac0ccfeb4a6338628f4
Joey Smith <jsmith@webinertia.net>
2024-02-11 15:09:48 -0600

    Improves TranslatorAwareInterfaceDelegatorFactory usage so that any service that either 1) Implements the interface directly or 2) Uses the implementing Trait If either is true then the delegated service will be decorated. Signed-off-by: Joey Smith <jsmith@webinertia.net>
    Signed-off-by: Joey Smith <jsmith@webinertia.net>

--- src/Service/Delegator/TranslatorAwareInterfaceDelegatorFactory.php
+++ src/Service/Delegator/TranslatorAwareInterfaceDelegatorFactory.php
@@ -6,29 +6,32 @@ namespace Axleus\Service\Delegator;
 
 use Laminas\I18n\Translator\TranslatorInterface;
 use Laminas\I18n\Translator\TranslatorAwareInterface;
+use Laminas\I18n\Translator\TranslatorAwareTrait;
 use Psr\Container\ContainerInterface;
 use Webmozart\Assert\Assert;
 
+use function class_uses;
+use function in_array;
 use function gettype;
 
 final class TranslatorAwareInterfaceDelegatorFactory
 {
-    public function __invoke(ContainerInterface $container, string $name, callable $callback): TranslatorAwareInterface
+    public function __invoke(ContainerInterface $container, string $name, callable $callback)
     {
         $service = $callback();
         if (! $container->has(TranslatorInterface::class)) {
             // if we do not have a translator, return the service as is.
             return $service;
         }
-        Assert::isInstanceOf(
-            $service,
-            TranslatorAwareInterface::class,
-            'Service must be an instance of '
-            . TranslatorAwareInterface::class
-            . ' to be delegated, received: ' . gettype($service)
-        );
-        // set the translator
-        $service->setTranslator($container->get(TranslatorInterface::class));
+
+        // Only decorate the service if either of these are true
+        if (
+            ($service instanceof TranslatorAwareInterface
+            || in_array(TranslatorAwareTrait::class, class_uses($service)))
+        ) {
+            $service->setTranslator($container->get(TranslatorInterface::class));
+        }
+
         return $service;
     }
 }
