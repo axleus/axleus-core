67e766e72ca13f222a9f2e9fa63f5481f8b445d7
Joey Smith <jsmith@webinertia.net>
2024-11-20 13:42:44 -0600

    Refactor complete Signed-off-by: Joey Smith <jsmith@webinertia.net>
    Signed-off-by: Joey Smith <jsmith@webinertia.net>

--- src/ConfigProvider.php
+++ src/ConfigProvider.php
@@ -4,11 +4,42 @@ declare(strict_types=1);
 
 namespace Axleus\Core;
 
-use Psr\Container\ContainerInterface;
+use Laminas\EventManager;
 
 final class ConfigProvider
 {
     public function __invoke(): array
+    {
+        return [
+            'dependencies' => $this->getDependencies(),
+            'listeners'    => $this->getListeners(),
+        ];
+    }
+
+    public function getDependencies(): array
+    {
+        return [
+            'aliases'    => [
+                EventManager\EventManagerInterface::class       => EventManager\EventManager::class,
+                'EventManager'                                  => EventManager\EventManager::class,
+                EventManager\SharedEventManagerInterface::class => EventManager\SharedEventManager::class,
+                'SharedEventManager'                            => EventManager\SharedEventManager::class,
+            ],
+            'delegators' => [
+                EventManager\EventManager::class => [
+                    Container\ListenerConfigurationDelegator::class,
+                ],
+            ],
+            'invokables' => [
+            ],
+            'factories'  => [
+                EventManager\EventManager::class       => Container\EventManagerFactory::class,
+                EventManager\SharedEventManager::class => static fn() => new EventManager\SharedEventManager(),
+            ],
+        ];
+    }
+
+    public function getListeners(): array
     {
         return [];
     }
