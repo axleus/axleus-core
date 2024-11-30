8b88970d7b594ba02becaf90953b640bfb263422
Joey Smith <jsmith@webinertia.net>
2024-11-29 19:47:04 -0600

    Awating feedback from the upstream Navigation developer before moving forward with navigation integration Signed-off-by: Joey Smith <jsmith@webinertia.net>
    Signed-off-by: Joey Smith <jsmith@webinertia.net>

--- src/Middleware/AuthorizedHandlerPipelineDelegator.php
+++ src/Middleware/AuthorizedHandlerPipelineDelegator.php
@@ -4,10 +4,13 @@ declare(strict_types=1);
 
 namespace Axleus\Core\Middleware;
 
+use Axleus\Authorization\Middleware\AuthorizationMiddleware;
 use Axleus\Core\Middleware\TemplateMiddleware;
 use Laminas\Stratigility\MiddlewarePipe;
 use Mezzio\MiddlewareFactory;
 use Mezzio\Session\SessionMiddleware;
+//use Mimmi20\Mezzio\GenericAuthorization\AuthorizationMiddleware;
+use Mimmi20\Mezzio\Navigation\NavigationMiddleware;
 use Psr\Container\ContainerInterface;
 
 use function class_exists;
@@ -25,8 +28,8 @@ final class AuthorizedHandlerPipelineDelegator
             $pipeline->pipe($factory->prepare(\Axleus\UserManager\Middleware\IdentityMiddleware::class));
         }
 
-        if (class_exists(\Mezzio\Authorization\AuthorizationMiddleware::class)) {
-            $pipeline->pipe($factory->prepare(\Mezzio\Authorization\AuthorizationMiddleware::class));
+        if (class_exists(AuthorizationMiddleware::class)) {
+            $pipeline->pipe($factory->prepare(AuthorizationMiddleware::class));
         }
 
         if (class_exists(\Axleus\Message\ConfigProvider::class)) {
@@ -41,6 +44,8 @@ final class AuthorizedHandlerPipelineDelegator
         }
 
         $pipeline->pipe($factory->prepare(TemplateMiddleware::class));
+
+        $pipeline->pipe($factory->prepare(NavigationMiddleware::class));
         $pipeline->pipe($factory->prepare($callback()));
 
         return $pipeline;
