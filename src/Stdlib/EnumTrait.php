0124db2b2b3e40405a70e27697e55aca76ddad0c
Joey Smith <jsmith@webinertia.net>
2024-12-24 16:49:22 -0600

    Signed-off-by: Joey Smith <jsmith@webinertia.net>

--- src/Stdlib/EnumTrait.php
+++ src/Stdlib/EnumTrait.php
@@ -7,9 +7,4 @@ namespace Axleus\Core\Stdlib;
 trait EnumTrait
 {
     use EnumToArrayTrait;
-
-    public static function noramalize(string $value): string
-    {
-        return self::isValid($value) ? $value : self::fromValue($value)->value;
-    }
 }
